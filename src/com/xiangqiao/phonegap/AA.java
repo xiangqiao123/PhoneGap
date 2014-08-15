f29b4749078c35d492f0cbd3631b24189d8cc2d4
项旻 <xiangqiao.xq@alibaba-inc.com>
2014-08-15 11:26:51 +0800

    init

---
+++ src/com/xiangqiao/phonegap/AA.java
@@ -0,0 +1,22 @@
+package com.xiangqiao.phonegap;
+
+import org.apache.cordova.DroidGap;
+
+import android.widget.Toast;
+
+public class AA {
+
+	DroidGap d;
+
+	public AA(DroidGap d) {
+		this.d = d;
+	}
+
+	public void show(String s) {
+		Toast.makeText(d.getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+	}
+
+	public String forAlert(String s) {
+		return "java_" + s;
+	}
+}
